6dce819f596c6514c0faae716a92e8c274924915
hina0629 <hina.0629.xxx@gmail.com>
2025-09-04 17:16:38 +0900

    0904_interface完了

--- 0904_3_interface/main.py
+++ 0904_3_interface/main.py
@@ -1,1 +1,27 @@
 # 0904_interface
+# インタフェース
+# 抽象基底クラスにて、
+# すべてが抽象メソッドのクラス。
+# つまり、口（くち）の定義だけで、
+# 実処理が一切ない。
+from abc import ABC, abstractmethod
+
+class Player(ABC):
+    @abstractmethod
+    def play(self):
+        pass
+
+    @abstractmethod
+    def stop(self):
+        pass
+
+class Myplayer(Player):
+    def play(self):
+        print("play")
+
+    def stop(self):
+        print("stop")
+
+mp = Myplayer()
+mp.play()
+mp.stop()
